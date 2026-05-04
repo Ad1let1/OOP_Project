8e540624d0e5b4eb725539661e7bab469b570e58
Adilet Sergazyuly <4d1let0@gmail.com>
2026-05-04 08:21:23 +0500

    Update TechSupportSpecialist.java

--- university/models/users/TechSupportSpecialist.java
+++ university/models/users/TechSupportSpecialist.java
@@ -9,20 +9,16 @@ public class TechSupportSpecialist extends Employee {
     public TechSupportSpecialist() {
     }
 
-    // Using ArrayList for requests as order of arrival is important
     public List<TechSupportRequest> requests = new ArrayList<>();
-
     public void receiveRequest(TechSupportRequest request) {
         requests.add(request);
     }
 
     public void viewRequests() {
-        // Using streams to cleanly print out all existing request states
         requests.stream().forEach(r -> System.out.println(r.state.getClass().getSimpleName()));
     }
 
     public void updateRequestStatus(TechSupportRequest request) {
-        // Advances the state by delegating to the state pattern's process method
         request.process();
     }
 
