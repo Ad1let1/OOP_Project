1599be368bd89eb6377e7bd89573027ac09c92ff
Adilet Sergazyuly <4d1let0@gmail.com>
2026-05-04 08:17:06 +0500

    Update ResearchProject.java

--- university/models/research/ResearchProject.java
+++ university/models/research/ResearchProject.java
@@ -3,22 +3,16 @@ package university.models.research;
 import university.exceptions.LowHIndexSupervisorException;
 import university.exceptions.NotAResearcherException;
 import university.models.users.User;
-
 import java.util.*;
 
 public class ResearchProject {
-
     public ResearchProject() {
     }
 
     public String topic;
-
     public Set<ResearchPaper> publishedPapers = new HashSet<>();
-
     public Set<User> participants = new HashSet<>();
-
     public void addParticipant(User p) throws NotAResearcherException {
-        // We strictly enforce only researcher types can join using runtime type checking
         if (!(p instanceof ResearcherDecorator)) {
             throw new NotAResearcherException("User " + p.username + " is not a Researcher.");
         }
@@ -26,7 +20,6 @@ public class ResearchProject {
     }
     
     public void setSupervisor(ResearcherDecorator supervisor) throws LowHIndexSupervisorException {
-        // Business rule requirement: A supervisor must have an h-index of at least 3.
         if (supervisor.calculateHIndex() < 3) {
             throw new LowHIndexSupervisorException("Supervisor must have an h-index >= 3");
         }
